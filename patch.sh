97fd37e6a65f1aed37f4065a8138b50dd283b056
Filip Kosecek <filip.kosecek@gmail.com>
2023-01-25 15:30:54 +0100

    Refactor Patch class
    TODO: handle errors, make sure the patch library gets closed

--- patch.sh
+++ patch.sh
@@ -1,65 +1,22 @@
 #!/bin/bash
 
-#TODO handle errors
-
-SECTION=".patch"
-DELIMITER=";"
-OUTFILE="${SECTION}-dump.bin"
-
-if [[ $# -ne 3 ]]; then
+if [[ $# -ne 2 ]]; then
 	echo "Enter the type of operation, pid and path to patch libary." 1<&2
 	exit 1
 fi
 
-if [[ $1 != "apply" ]]; then
-	echo "Only patch application is for now supported." 1<&2
-	exit 1
-fi
-
 #check if pid is valid
-if [[ -z $( ps -e | grep $2 ) ]]; then
+if [[ -z $( ps -e | grep $1 ) ]]; then
 	echo "Pid is not valid." 1<&2
-	exit 1
-fi
-
-#check if there is a patch section in patch library
-objdump -s -j $SECTION $3 > /dev/null
-if [[ $? -ne 0 ]]; then
-	echo "Could't find .patch section in the patch library." 1<&2
-	exit 1
-fi
-
-#extract data from .patch section
-objcopy --dump-section $SECTION=$OUTFILE $3
-META=$( cat $OUTFILE )
-rm $OUTFILE
-if [[ -z $META ]];then
-	echo "Invalid metadata in .patch section." 1<&2
-	exit 1
-fi
-
-#TODO check if characters are printable
-#extract metadata
-TYPE=$( echo $META | cut -d ':' -f 1 )
-OLD=$( echo $META | cut -d ':' -f 2 )
-NEW=$( echo $META | cut -d ':' -f 3 )
-
-if [[ $TYPE != "O" && $TYPE != "L" ]] || [[ -z $OLD || -z $NEW ]];then
-	echo "Invalid metadata format." 1<&2
 	exit 1
 fi
 
 #create command file for gdb
 COMMANDS=".tmp.gdb"
-echo "source $( pwd )/src/commands.py" >> $COMMANDS
-if [[ $TYPE == "O" ]]; then
-	echo -n "patch own " >> $COMMANDS
-else
-	echo -n "patch lib " >> $COMMANDS
-fi
-echo "\"$3\" ${OLD} ${NEW}" >> $COMMANDS
+echo "source $( pwd )/src/commands.py" > $COMMANDS
+echo "patch $2" >> $COMMANDS
 echo "detach" >> $COMMANDS
 
-gdb -p $2 --batch --command=$COMMANDS
+gdb -p $1 --batch --command=$COMMANDS > /dev/null
 
 rm $COMMANDS
--- src/commands.py
+++ src/commands.py
@@ -3,18 +3,12 @@ import gdb
 type_list = ["char", "uint64_t", "int32_t"]
 
 class Patch (gdb.Command):
-    "Prefix command for live patching functions."
-
-    def __init__(self):
-        super(Patch, self).__init__("patch", gdb.COMMAND_SUPPORT, gdb.COMPLETE_NONE, True)
-
-class PatchOwn (gdb.Command):
-    "Patch your own functions."
+    "Patch your functions."
 
     type_dict = {}
 
     def __init__(self):
-        super(PatchOwn, self).__init__("patch own", gdb.COMMAND_USER)
+        super(Patch, self).__init__("patch", gdb.COMMAND_USER)
 
     def find_patch_section_range(self) -> tuple[int, int]:
         output = gdb.execute("maintenance info sections -all-objects .patch", False, True)
@@ -50,8 +44,7 @@ class PatchOwn (gdb.Command):
         while output[section_end_index] != ' ':
             section_end += output[section_end_index]
             section_end_index += 1
-
-
+        
         beg = int(section_beg, base=16)
         end = int(section_end, base=16)
         #assuming there is a null terminating character
@@ -70,49 +63,28 @@ class PatchOwn (gdb.Command):
         else:
             return obj_addr
 
-    def is_patchable(self, target_addr: gdb.Value):
+    def is_patchable(self):
         try:
             #check if essential types are present in the target process
             for t in type_list:
                 self.type_dict[t] = gdb.lookup_type(t)
-
-            tmp = int(target_addr.cast(self.type_dict["uint64_t"]))
-            rip = int(gdb.parse_and_eval("$rip").cast(self.type_dict["uint64_t"]))
         except:
             raise gdb.GdbError("Required types were not supported.")
-        else:
-            if rip >= tmp and rip < tmp + 13:
-                raise gdb.GdbError("The code segment where the trampoline is about to be inserted is being executed.")
-
-    def load_patch_lib(self, dlopen_addr: gdb.Value, path: str, patch_function: str) -> gdb.Value:
-        dlopen_ret = dlopen_addr(path, 2)
+ 
+    def load_patch_lib(self, path: str):
+        dlopen_ret = self.dlopen_addr(path, 2)
         if dlopen_ret == 0:
             raise gdb.GdbError("Couldn't open the patch library.")
-        
-        try:
-            patch_addr = self.find_object(patch_function)
-        except:
-            dlclose = self.find_object("dlclose")
-            dlclose(dlopen_ret)
-            raise gdb.GdbError("Couldn't find the patch function.")
-        else:
-            return patch_addr
+       
+    def apply_patchown(self):
+        target_addr = self.find_object(self.target_func)
+        tmp = int(target_addr.cast(self.type_dict["uint64_t"]))
+        rip = int(gdb.parse_and_eval("$rip").cast(self.type_dict["uint64_t"]))
+        if rip >= tmp and rip < tmp + 13:
+            raise gdb.GdbError("The code segment where the trampoline is about to be inserted is being executed.")
 
-    def invoke(self, arg, from_tty):
-        self.type_dict.clear()
-        argv = gdb.string_to_argv(arg)
-        if len(argv) != 3:
-            raise gdb.GdbError("patch own takes three parameters")
-
-        #find necessary objects
-        dlopen_addr = self.find_object("dlopen")
-        target_addr = self.find_object(argv[1])
         trampoline = bytearray.fromhex("49 bb 00 00 00 00 00 00 00 00 41 ff e3")
-
-        self.is_patchable(target_addr)
-        patch_addr = self.load_patch_lib(dlopen_addr, argv[0], argv[2])
-
-        patch_addr = patch_addr.cast(self.type_dict["uint64_t"])
+        patch_addr = self.find_object(self.patch_func).cast(self.type_dict["uint64_t"])
         patch_addr_arr = int(patch_addr).to_bytes(8, byteorder = "little")
 
         for i in range(8):
@@ -123,46 +95,11 @@ class PatchOwn (gdb.Command):
         inferior = gdb.selected_inferior()
         inferior.write_memory(target_addr, trampoline, 13)
 
-        tup = self.find_patch_section_range()
-        metadata = self.extract_patch_metadata(tup[0], tup[1])
-
-class PatchLib (gdb.Command):
-    "Patch library function."
-
-    def __init__(self):
-        super(PatchLib, self).__init__("patch lib", gdb.COMMAND_USER)
-
-    def find_object(self, obj: str) -> gdb.Value:
-        try:
-            obj_addr = gdb.parse_and_eval(obj)
-        except:
-            raise gdb.GdbError("Couldn't find " + obj + ".")
-        else:
-            return obj_addr
-
-    def load_patch_lib(self, dlopen_addr: gdb.Value, path: str, patch_function: str) -> gdb.Value:
-        dlopen_ret = dlopen_addr(path, 2)
-        if dlopen_ret == 0:
-            raise gdb.GdbError("Couldn't open the patch library.")
-        try:
-            patch_addr = self.find_object(patch_function)
-        except:
-            dlclose = self.find_object("dlclose")
-            dlclose(dlopen_ret)
-            raise gdb.GdbError("Couldn't find the patch function.")
-        else:
-            return patch_addr
-
-    def invoke(self, arg, from_tty):
-        argv = gdb.string_to_argv(arg)
-        if len(argv) != 3:
-            raise gdb.GdbError("patch own takes three parameters")
-
-        inferior = gdb.selected_inferior()
-
-        dlopen = self.find_object("dlopen")
-        target = self.find_object("'" + argv[1] + "@plt'")
-        patch = self.load_patch_lib(dlopen, argv[0], argv[2])
+    def apply_patchlib(self):
+        #find target and patch functions
+        target = "'" + self.target_func + "@plt'"
+        target = self.find_object(target)
+        patch = self.find_object(self.patch_func)
         patch = patch.cast(gdb.lookup_type("char").pointer())
 
         #fetch relative offset
@@ -181,9 +118,30 @@ class PatchLib (gdb.Command):
         patch_arr = int(patch).to_bytes(8, byteorder = "little")
 
         #write patch function address
+        inferior = gdb.selected_inferior()
         inferior.write_memory(addr_got, patch_arr, 8)
-        
+
+    def invoke(self, arg, from_tty):
+        self.type_dict.clear()
+        argv = gdb.string_to_argv(arg)
+        if len(argv) != 1:
+            raise gdb.GdbError("patch own takes three parameters")
+
+        #find necessary objects
+        self.dlopen_addr = self.find_object("dlopen")
+        self.dlclose_addr = self.find_object("dlclose")
+        self.is_patchable()
+        self.load_patch_lib(argv[0])
+        span = self.find_patch_section_range()
+        metadata = self.extract_patch_metadata(span[0], span[1])
+        self.target_func = metadata[1]
+        self.patch_func = metadata[2]
+
+        if metadata[0] == 'O':
+            self.apply_patchown()
+        elif metadata[0] == 'L':
+            self.apply_patchlib()
+        else:
+            raise gdb.GdbError("Patching own and library functions is only supoorted for now.")
 
 Patch()
-PatchOwn()
-PatchLib()
